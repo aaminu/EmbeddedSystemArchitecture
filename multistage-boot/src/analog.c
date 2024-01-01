9e52106c8d46cf08adcb304da89e6063e26389dc
aaminu <aminuabdulrahmandayo@gmail.com>
2024-01-01 09:10:56 +0100

    Refactoring ADC module amd FPU initialization

--- multistage-boot/src/analog.c
+++ multistage-boot/src/analog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "analog.h"
 #include "system.h"
 
@@ -11,11 +12,16 @@
 
 /******ADC*******/
 #define ADC1_BASE (0x40012000)
-#define ADC2_BASE (0x40012000 + 0x100)
-#define ADC3_BASE (0x40012000 + 0x200)
-#define ADC_COMMON_REG_BASE (0x40012000 + 0x300)
+#define ADC2_BASE (ADC1_BASE + 0x100)
+#define ADC3_BASE (ADC1_BASE + 0x200)
+#define ADC_COMMON_REG_BASE (ADC1_BASE + 0x300)
+#define SELECT_ADC_CONTR_BASE(controller)                                \
+    ((controller == ADC1) ? ADC1_BASE : (controller == ADC2) ? ADC2_BASE \
+                                    : (controller == ADC3)   ? ADC3_BASE \
+                                                             : 0)
 
 /******ADC REG OFFSETS********/
+
 #define ADC_SR (0x00)
 #define ADC_CR1 (0x04)
 #define ADC_CR2 (0x08)
@@ -27,24 +33,30 @@
 #define ADC_SQR3 (0x34)
 #define ADC_DR (0x4C)
 
-#define ADC_CSR (0x00)
-#define ADC_CCR (0x04)
-#define ADC_CDR (0x08)
+/**********ADC COMMON REG*************/
+#define ADC_CSR (*(volatile uint32_t *)(ADC_COMMON_REG_BASE + 0x00))
+#define ADC_CCR (*(volatile uint32_t *)(ADC_COMMON_REG_BASE + 0x04))
+#define ADC_CDR (*(volatile uint32_t *)(ADC_COMMON_REG_BASE + 0x08))
 
 /*****FLAGS*****/
+
 #define ADC_SR_EOC (1 << 1)       // Regular channel end of conversion
 #define ADC_CR1_SCAN (1 << 8)     // Scan mode enabled
+#define ADC_CR1_DISCEN (1 << 11)  // Discontinuous mode on regular channels
 #define ADC_CR2_ADON (1 << 0)     // ADC On
 #define ADC_CR2_CONT (1 << 1)     // Continous conversion enabled
 #define ADC_CR2_SWSTART (1 << 30) // Start conversion of regular channel
 
 /***ADC Prescalers***/
-#define ADC_CCR_ADCPRE_PCLK_DIV2 (0x0 << 16)
-#define ADC_CCR_ADCPRE_PCLK_DIV4 (0x1 << 16)
-#define ADC_CCR_ADCPRE_PCLK_DIV6 (0x2 << 16)
-#define ADC_CCR_ADCPRE_PCLK_DIV8 (0x3 << 16)
+
+#define ADC_CCR_ADCPRE_PCLK_DIV2 (0x00 << 16)
+#define ADC_CCR_ADCPRE_PCLK_DIV4 (0x01 << 16)
+#define ADC_CCR_ADCPRE_PCLK_DIV6 (0x02 << 16)
+#define ADC_CCR_ADCPRE_PCLK_DIV8 (0x03 << 16)
+#define ADC_CCR_TSVREFE (0x01 << 23)
 
 /*****SAMPLING TIME*****/
+
 #define ADC_SMPR_3_CYCLE (0x00)
 #define ADC_SMPR_15_CYCLE (0x01)
 #define ADC_SMPR_28_CYCLE (0x02)
@@ -54,14 +66,32 @@
 #define ADC_SMPR_144_CYCLE (0x06)
 #define ADC_SMPR_480_CYCLE (0x07)
 
+#define SELECT_SMP(smp)                                                                                         \
+    ((smp == ADC_SAMPLETIME_3CYCLES) ? ADC_SMPR_3_CYCLE : (smp == ADC_SAMPLETIME_15CYCLES) ? ADC_SMPR_15_CYCLE  \
+                                                      : (smp == ADC_SAMPLETIME_28CYCLES)   ? ADC_SMPR_28_CYCLE  \
+                                                      : (smp == ADC_SAMPLETIME_56CYCLES)   ? ADC_SMPR_56_CYCLE  \
+                                                      : (smp == ADC_SAMPLETIME_84CYCLES)   ? ADC_SMPR_84_CYCLE  \
+                                                      : (smp == ADC_SAMPLETIME_112CYCLES)  ? ADC_SMPR_112_CYCLE \
+                                                      : (smp == ADC_SAMPLETIME_144CYCLES)  ? ADC_SMPR_144_CYCLE \
+                                                                                           : ADC_SMPR_480_CYCLE)
+
 /***DATA ALIGN*********/
+
 #define ADC_CR2_ALIGNR (0 << 11)
 #define ADC_CR2_ALIGNL (1 << 11)
 
 /********OTHERS*********/
+#define TEMPERATURE_CHANNEL (16)
 #define CHANNEL_ERROR (100)
+#define ANALOG_VAL_ERROR (1)
+#define TEMP_VAL_ERROR (50.0)
 
-/**************Static Function**************************/
+/**************Statics**************************/
+
+#define IS_ADC1_CLK_EN (0x01)
+#define IS_ADC2_CLK_EN (0x02)
+#define IS_ADC3_CLK_EN (0x04)
+static uint8_t is_adc_controller_clk_en = 0; // Keep track of ADC controller clock enabled, 0x01(ADC1), 0x02(ADC2), 0x40(ADC3)
 
 /**
  * @brief Function returns the required channel for ADC usagae. The
@@ -74,41 +104,179 @@ static int8_t adc_channel(gpio_dt_spec *pin_spec)
 {
     int channel = -CHANNEL_ERROR;
 
-    switch (pin_spec->port)
+    if ((pin_spec->port == PORT_A) && (pin_spec->pin_number <= 7))
     {
-    case PORT_A:
-        if (pin_spec->pin_number <= 7)
-        {
-            channel = pin_spec->pin_number;
-            break;
-        }
-        channel = -CHANNEL_ERROR;
-        break;
-
-    case PORT_B:
-        if (pin_spec->pin_number <= 1)
-        {
-            channel = pin_spec->pin_number + 8;
-            break;
-        }
-        channel = -CHANNEL_ERROR;
-        break;
-
-    case PORT_C:
-        if (pin_spec->pin_number <= 5)
-        {
-            channel = pin_spec->pin_number + 10;
-            break;
-        }
-        channel = -CHANNEL_ERROR;
-        break;
-
-    default:
-        channel = -CHANNEL_ERROR;
-        break;
+        channel = pin_spec->pin_number;
+    }
+    else if ((pin_spec->port == PORT_B) && (pin_spec->pin_number <= 1))
+    {
+        channel = pin_spec->pin_number + 8;
+    }
+    else if ((pin_spec->port == PORT_C) && (pin_spec->pin_number <= 5))
+    {
+        channel = pin_spec->pin_number + 10;
     }
 
     return channel;
+}
+
+static void adc_controller_clk_en(adc_controller_t adc_controller)
+{
+    if ((adc_controller == ADC1) && ((is_adc_controller_clk_en & IS_ADC1_CLK_EN) == 0))
+    {
+        RCC_APB2ENR |= ADC1EN;
+        is_adc_controller_clk_en |= IS_ADC1_CLK_EN;
+    }
+    else if ((adc_controller == ADC2) && ((is_adc_controller_clk_en & IS_ADC2_CLK_EN) == 0))
+    {
+        RCC_APB2ENR |= ADC2EN;
+        is_adc_controller_clk_en |= IS_ADC2_CLK_EN;
+    }
+    else if ((adc_controller == ADC3) && ((is_adc_controller_clk_en & IS_ADC3_CLK_EN) == 0))
+    {
+        RCC_APB2ENR |= ADC3EN;
+        is_adc_controller_clk_en |= IS_ADC3_CLK_EN;
+    }
+}
+
+static void set_adc_prescaler(adc_clock_prescaler_t adc_prescaler)
+{
+    // Clear the reg
+    ADC_CCR &= ~(0x03 << 16);
+
+    switch (adc_prescaler)
+    {
+    case ADC_PCLK_DIV2:
+        ADC_CCR |= ADC_CCR_ADCPRE_PCLK_DIV2;
+        break;
+    case ADC_PCLK_DIV4:
+        ADC_CCR |= ADC_CCR_ADCPRE_PCLK_DIV4;
+        break;
+    case ADC_PCLK_DIV6:
+        ADC_CCR |= ADC_CCR_ADCPRE_PCLK_DIV6;
+        break;
+    case ADC_PCLK_DIV8:
+        ADC_CCR |= ADC_CCR_ADCPRE_PCLK_DIV8;
+        break;
+    default:
+        break;
+    }
+}
+
+static void adc_mode_set(adc_controller_t adc_controller, adc_mode_t mode, adc_mode_trigger_t en)
+{
+    uint32_t controller_base = SELECT_ADC_CONTR_BASE(adc_controller);
+    if (!controller_base)
+        return;
+
+    switch (mode)
+    {
+    case ADC_SINGLE_MODE:
+        (*(volatile uint32_t *)(controller_base + ADC_CR2)) &= ~(ADC_CR2_CONT);
+        break;
+
+    case ADC_CONTINOUS_MODE:
+        if (en)
+        {
+            (*(volatile uint32_t *)(controller_base + ADC_CR2)) |= ADC_CR2_CONT;
+            break;
+        }
+
+        (*(volatile uint32_t *)(controller_base + ADC_CR2)) &= ~(ADC_CR2_CONT);
+        break;
+
+    case ADC_SCAN_MODE:
+        if (en)
+        {
+            (*(volatile uint32_t *)(controller_base + ADC_CR1)) |= ADC_CR1_SCAN;
+            break;
+        }
+
+        (*(volatile uint32_t *)(controller_base + ADC_CR1)) &= ~(ADC_CR1_SCAN);
+        break;
+
+    case ADC_DISCONTINOUS_MODE:
+        if (en)
+        {
+            (*(volatile uint32_t *)(controller_base + ADC_CR1)) |= ADC_CR1_DISCEN;
+            break;
+        }
+
+        (*(volatile uint32_t *)(controller_base + ADC_CR1)) &= ~(ADC_CR1_DISCEN);
+        break;
+
+    default:
+        break;
+    }
+}
+
+static void adc_sampletime_set(adc_controller_t adc_controller, adc_sampletime_t sample_time, uint8_t channel)
+{
+    uint32_t controller_base = SELECT_ADC_CONTR_BASE(adc_controller);
+    if (!controller_base)
+        return;
+
+    if (channel < 9)
+    {
+        // clear
+        (*(volatile uint32_t *)(controller_base + ADC_SMPR2)) &= ~(0x07 << (channel * 3));
+        (*(volatile uint32_t *)(controller_base + ADC_SMPR2)) |= (SELECT_SMP(sample_time) << (channel * 3));
+    }
+    else
+    {
+        // clear
+        (*(volatile uint32_t *)(controller_base + ADC_SMPR1)) &= ~(0x07 << ((channel - 10) * 3));
+        (*(volatile uint32_t *)(controller_base + ADC_SMPR1)) |= (SELECT_SMP(sample_time) << ((channel - 10) * 3));
+    }
+}
+
+static void adc_reg_switch(adc_controller_t adc_controller, adc_mode_trigger_t en)
+{
+    uint32_t controller_base = SELECT_ADC_CONTR_BASE(adc_controller);
+    if (!controller_base)
+        return;
+
+    if (en)
+    {
+        (*(volatile uint32_t *)(controller_base + ADC_CR2)) |= ADC_CR2_ADON;
+        return;
+    }
+
+    (*(volatile uint32_t *)(controller_base + ADC_CR2)) &= ~(ADC_CR2_ADON);
+}
+
+static int adc_singleconv_read(adc_controller_t adc_controller, uint8_t channel)
+{
+
+    uint32_t controller_base = SELECT_ADC_CONTR_BASE(adc_controller);
+    if (!controller_base)
+        return -1;
+
+    // Disable ADC
+    adc_reg_switch(ADC1, DISABLE);
+
+    // Set Channel to SQR_x
+    (*(volatile uint32_t *)(controller_base + ADC_SQR3)) |= channel;
+
+    // Enable ADC
+    adc_reg_switch(ADC1, ENABLE);
+
+    // Start Conversion
+    (*(volatile uint32_t *)(controller_base + ADC_CR2)) |= ADC_CR2_SWSTART;
+
+    // Wait for Conversion to Start
+    while ((*(volatile uint32_t *)(controller_base + ADC_CR2)) & ADC_CR2_SWSTART)
+    {
+        ;
+    }
+
+    // Wait for End of Conversion
+    while (((*(volatile uint32_t *)(controller_base + ADC_SR)) & ADC_SR_EOC) == 0)
+    {
+        ;
+    }
+
+    return (int)(*(volatile uint32_t *)(controller_base + ADC_DR));
 }
 
 /****************************************/
@@ -123,41 +291,29 @@ void adc_init(gpio_dt_spec *pin_spec)
     if (channel < 0)
         return;
 
-    // Enable ADC CLK
-    RCC_APB2ENR |= ADC1EN;
+    // check and Enable ADC CLK if required
+    adc_controller_clk_en(ADC1);
 
     // Initial GPIO as ADC
     gpio_init(pin_spec);
 
     // Disable ADC
-    (*(volatile uint32_t *)(ADC1_BASE + ADC_CR2)) &= ~(ADC_CR2_ADON);
+    adc_reg_switch(ADC1, DISABLE);
 
-    // Set the Clock Prescaler (clear amd set)
-    (*(volatile uint32_t *)(ADC_COMMON_REG_BASE + ADC_CCR)) &= ~(0x03 << 16);
-    (*(volatile uint32_t *)(ADC_COMMON_REG_BASE + ADC_CCR)) |= ADC_CCR_ADCPRE_PCLK_DIV2;
+    // Set the Clock Prescaler
+    set_adc_prescaler(ADC_PCLK_DIV2);
 
     // Disable Scan Mode
-    (*(volatile uint32_t *)(ADC1_BASE + ADC_CR1)) &= ~(ADC_CR1_SCAN);
+    adc_mode_set(ADC1, ADC_SCAN_MODE, DISABLE);
 
     // Disable Continous Mode
-    (*(volatile uint32_t *)(ADC1_BASE + ADC_CR2)) &= ~(ADC_CR2_CONT);
+    adc_mode_set(ADC1, ADC_SINGLE_MODE, ENABLE);
 
     // Sampling Frequency
-    if (channel < 9)
-    {
-        // clear
-        (*(volatile uint32_t *)(ADC1_BASE + ADC_SMPR2)) &= ~(0x07 << (channel * 3));
-        (*(volatile uint32_t *)(ADC1_BASE + ADC_SMPR2)) |= (ADC_SMPR_480_CYCLE << (channel * 3));
-    }
-    else
-    {
-        // clear
-        (*(volatile uint32_t *)(ADC1_BASE + ADC_SMPR1)) &= ~(0x07 << ((channel - 10) * 3));
-        (*(volatile uint32_t *)(ADC1_BASE + ADC_SMPR1)) |= (ADC_SMPR_480_CYCLE << ((channel - 10) * 3));
-    }
+    adc_sampletime_set(ADC1, ADC_SAMPLETIME_480CYCLES, channel);
 
     // Enable ADC
-    (*(volatile uint32_t *)(ADC1_BASE + ADC_CR2)) |= ADC_CR2_ADON;
+    adc_reg_switch(ADC1, ENABLE);
 }
 
 int adc_read(gpio_dt_spec *pin_spec)
@@ -165,41 +321,53 @@ int adc_read(gpio_dt_spec *pin_spec)
     // Check if Pin is suitable for ADC
     int channel = adc_channel(pin_spec);
     if (channel < 0)
-        return -1;
+        return -ANALOG_VAL_ERROR;
 
-    // Disable ADC
-    (*(volatile uint32_t *)(ADC1_BASE + ADC_CR2)) &= ~(ADC_CR2_ADON);
-
-    // Set Channel to SQR_x
-    (*(volatile uint32_t *)(ADC1_BASE + ADC_SQR3)) |= channel;
-
-    // Enable ADC
-    (*(volatile uint32_t *)(ADC1_BASE + ADC_CR2)) |= ADC_CR2_ADON;
-
-    // Start Conversion
-    (*(volatile uint32_t *)(ADC1_BASE + ADC_CR2)) |= ADC_CR2_SWSTART;
-
-    // Wait for Conversion to Start
-    while ((*(volatile uint32_t *)(ADC1_BASE + ADC_CR2)) & ADC_CR2_SWSTART)
-    {
-        ;
-    }
-
-    // Wait for End of Conversion
-    while (((*(volatile uint32_t *)(ADC1_BASE + ADC_SR)) & ADC_SR_EOC) == 0)
-    {
-        ;
-    }
-
-    return (int)(*(volatile uint32_t *)(ADC1_BASE + ADC_DR));
+    return adc_singleconv_read(ADC1, channel);
 }
 
-double adc_read_volt(gpio_dt_spec *pin_spec)
-{
-    int value = adc_read(pin_spec);
+// float adc_read_volt(gpio_dt_spec *pin_spec)
+// {
+//     int value = adc_read(pin_spec);
 
-    if (value < 0)
-        return -1.0;
+//     if (value < 0)
+//         return -1.0;
 
-    return (3.3 * value) / (4095.0);
-}
+//     return (2.79 * value) / (4095.0);
+// }
+
+// float adc_temperature(void)
+// {
+
+//     // check and Enable ADC CLK if required
+//     adc_controller_clk_en(ADC1);
+
+//     // Disable ADC
+//     adc_reg_switch(ADC1, DISABLE);
+
+//     // Set the Clock Prescaler
+//     set_adc_prescaler(ADC_PCLK_DIV2);
+
+//     // Disable Scan Mode
+//     adc_mode_set(ADC1, ADC_SCAN_MODE, DISABLE);
+
+//     // Disable Continous Mode
+//     adc_mode_set(ADC1, ADC_SINGLE_MODE, ENABLE);
+
+//     // Sampling Frequency, Temp Sensor is on ADC_IN16
+//     adc_sampletime_set(ADC1, ADC_SAMPLETIME_480CYCLES, TEMPERATURE_CHANNEL);
+
+//     // Enable ADC_CCR TSVREFE
+//     ADC_CCR |= ADC_CCR_TSVREFE;
+
+//     // Enable ADC
+//     adc_reg_switch(ADC1, ENABLE);
+
+//     int value = adc_singleconv_read(ADC1, TEMPERATURE_CHANNEL);
+//     if (value == -ANALOG_VAL_ERROR)
+//         return -TEMP_VAL_ERROR;
+
+//     double v_sense = (2.79 * value) / (4095.0);
+
+//     return ((v_sense - 0.76) * 100) / (2.5) + 25;
+// }
--- multistage-boot/src/analog.h
+++ multistage-boot/src/analog.h
@@ -3,7 +3,46 @@
 
 #include "gpios.h"
 
-/*ADC RESOLUTION*/
+typedef enum
+{
+    ADC1,
+    ADC2,
+    ADC3
+} adc_controller_t;
+
+typedef enum
+{
+    ADC_PCLK_DIV2,
+    ADC_PCLK_DIV4,
+    ADC_PCLK_DIV6,
+    ADC_PCLK_DIV8
+} adc_clock_prescaler_t;
+
+typedef enum
+{
+    ADC_SINGLE_MODE,
+    ADC_CONTINOUS_MODE,
+    ADC_SCAN_MODE,
+    ADC_DISCONTINOUS_MODE = 4
+} adc_mode_t;
+
+typedef enum
+{
+    DISABLE,
+    ENABLE
+} adc_mode_trigger_t;
+
+typedef enum
+{
+    ADC_SAMPLETIME_3CYCLES,
+    ADC_SAMPLETIME_15CYCLES,
+    ADC_SAMPLETIME_28CYCLES,
+    ADC_SAMPLETIME_56CYCLES,
+    ADC_SAMPLETIME_84CYCLES,
+    ADC_SAMPLETIME_112CYCLES,
+    ADC_SAMPLETIME_144CYCLES,
+    ADC_SAMPLETIME_480CYCLES,
+} adc_sampletime_t;
 
 /**
  * @brief Enables the ADC and selects the required channel
@@ -23,14 +62,16 @@ void adc_init(gpio_dt_spec *pin_spec);
  */
 int adc_read(gpio_dt_spec *pin_spec);
 
-/**
- * @brief Reads and returns the volatage level on the pin
- * @param pin_spec gpio_dt_spec pointer to gpio config struct containing
- * port name, pin number and mode.
- *
- * @retval 0 <= x <= 3.30v
- * @retval < 0 for Error
- */
-double adc_read_volt(gpio_dt_spec *pin_spec);
+// /**
+//  * @brief Reads and returns the volatage level on the pin
+//  * @param pin_spec gpio_dt_spec pointer to gpio config struct containing
+//  * port name, pin number and mode.
+//  *
+//  * @retval 0 <= x <= 3.30v
+//  * @retval < 0 for Error
+//  */
+// float adc_read_volt(gpio_dt_spec *pin_spec);
+
+// float adc_temperature(void);
 
 #endif /*ANALOG_H_*/
