ff9d697cd6f5ae60999173ab2721919f09fc41f1
aaminu <aminuabdulrahmandayo@gmail.com>
2024-01-07 15:59:12 +0100

    Refactoring Folders ti create examples

--- multistage-boot/examples/1_gpios/main.c
+++ multistage-boot/examples/1_gpios/main.c
@@ -4,7 +4,6 @@
 #include "system.h"
 #include "systick.h"
 #include "gpios.h"
-#include "analog.h"
 
 static int bss_variable;
 static int initialized_variable = 31;
@@ -13,10 +12,9 @@ extern int mpu_enable(void);
 void sys_init(void);
 void toggle_led5(void);
 
-// GPIOS
-gpio_dt_spec led = U_LED3;
-gpio_dt_spec led1 = U_LED4;
-gpio_dt_spec led2 = U_LED5;
+// Define GPIOS, the Macros are gpios available on the board
+gpio_dt_spec led3 = U_LED3;
+gpio_dt_spec led4 = U_LED4;
 gpio_dt_spec button = U_BUTN;
 
 void main(void)
@@ -24,56 +22,38 @@ void main(void)
     // Initialize System
     sys_init();
 
-    /*Setup GPIOS*/
-    gpio_dt_spec input = {PORT_C, 11, GPIO_INPUT};
-
-    gpio_init(&led);
-    gpio_init(&led1);
-    gpio_init(&led2);
+    /*Init GPIOS*/
+    gpio_init(&led3);
+    gpio_init(&led4);
     gpio_init(&button);
-    gpio_init_with_irq(&input, RISING_EDGE, toggle_led5);
 
-    gpio_set(&led, 1);
-    gpio_set(&led1, 0);
-    gpio_set(&led1, 0);
+    /*Set values to the gpios*/
+    gpio_set(&led3, 1);
+    gpio_set(&led4, 0);
 
+    /*System Tick for debaouncing*/
     unsigned int ticks = millis();
     unsigned int debounce = 0;
 
-    // ADC
-    gpio_dt_spec adc_pin = {PORT_A, 5, GPIO_ANALOG};
-    adc_init(&adc_pin);
-    int adc_value = adc_read(&adc_pin);
-    int adc_volt = adc_read_volt(&adc_pin);
-    int temperature = adc_temperature();
-    float a = (1.0 + 2.0) * 1.20f;
-
     while (1)
     {
-        // char *c = (char *)malloc(10);
         bss_variable++;
         initialized_variable++;
 
-        // free(c);
-
+        // Blink LD3 on the board atleast after a second
         if ((millis() - ticks) / 1000 >= 1)
         {
-            gpio_toggle(&led);
+            gpio_toggle(&led3);
             ticks = millis();
-            adc_value = adc_read(&adc_pin);
         }
 
+        // Only toggle the LD4  when button is pressed. Button is debounced to remove transient value.
         if (gpio_value(&button) && (millis() - debounce) > 500)
         {
-            gpio_toggle(&led1);
+            gpio_toggle(&led4);
             debounce = millis();
         }
     }
-}
-
-void toggle_led5(void)
-{
-    gpio_toggle(&led2);
 }
 
 void sys_init(void)
